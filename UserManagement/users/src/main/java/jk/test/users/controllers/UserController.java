203fa801c1edbd7dfab7b85e8fc5c2c078b0f3c3
Jani Kahari <janikl17gg@gmail.com>
2023-04-28 18:09:13 +0300

    Add: update

--- UserManagement/users/src/main/java/jk/test/users/controllers/UserController.java
+++ UserManagement/users/src/main/java/jk/test/users/controllers/UserController.java
@@ -1,12 +1,14 @@
 package jk.test.users.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,5 +38,15 @@ public class UserController {
 	@DeleteMapping("/user/{id}")
 	void deleteUser(@PathVariable Integer id) {
 		repository.deleteById(id);
+	}
+	
+	@PutMapping("/user/{id}")
+	Optional<User> updateUser(@RequestBody User updatedUser, @PathVariable Integer id) {
+		return repository.findById(id)
+			      .map(user -> {
+			    	  user.setName(updatedUser.getName());
+			    	  user.setPhone(updatedUser.getPhone());
+			        return repository.save(user);
+			      });
 	}
 }
